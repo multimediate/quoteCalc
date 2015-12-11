7b2ff191ed5a0552f1b2abd9eddc87ca7e63503e
multimediate <jake@multimediate.us>
2015-12-11 10:33:56 -0700

    v 1.2.1
    Added header/footer; updated GenQuote functionality; changed radio
    buttons; added dropdown visibility for store fields.

--- js/quoteCalc.js
+++ js/quoteCalc.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////
 // ABOUT: Quote Calculator
 // CREATED BY: Jacob Wiedmann
-// VERSION: 1.2
+// VERSION: 1.2.1
 // COPYRIGHT: GoDaddy Operating Company, LLC All Rights Reserved
 ////////////////////////////////////////////////////////////
 
@@ -11,44 +11,34 @@
 //			1. Add value="XXX" to input
 //			2. Add onclick="startCalc(); to input
 // ============================================
+
+
 var radioTotal = 0;
 
-$('.radio-inline').click(function(){
-	
+$('.radio-inline').change(function(){
 	var Total = 0;
-	
-	$('.option:checked').each(function() {
+
+	$('.active .option').each(function() {
 		Total += parseInt($(this).val());
 	});
 
 	radioTotal = Total;
+	
+	startCalc();
+
 });
 
-//Store form field toggle
-// $(document).ready(function(){
-// 	$('.productCount').hide();
-// 	$('.optionSetCount').hide();
+// if ($('#storeYes').parent().hasClass('.active') {
+// 	$('#storeChildren').collapse('toggle');
+// 	$('#storeChildren').addClass("in");
+// } else {
+// 	$('#storeChildren').collapse('hide');
+// 	$('#storeChildren').removeClass("in");
+// };
 
-// 	$('.radio-inline').click(function(){
-// 		if ($('#storeYes:checked')) {
-// 			$('.productCount').show();
-// 			$('.optionSetCount').show();
-// 		} else {
-// 			$('.productCount').hide();
-// 			$('.optionSetCount').hide();
-// 		};
-// 	});
-// });
 
-// $(document).ready(function() {
-// 	$('.radio-inline').click(function() {
-// 		if($("#storeYes.option:checked")  {
-// 			$('#storeChildren').show();
-// 		} else {
-// 			$('#storeChildren').hide();
-// 		}
-// 	});
-// });
+
+
 
 
 // ============================================
@@ -56,7 +46,7 @@ $('.radio-inline').click(function(){
 // ============================================
 
 // Initialize Variables
-var baseline = 15; //Initial Setup of 15 minutes
+var baseline = 10; //Initial Setup of 10 minutes
 var one = 0;
 var two = 0;
 var three = 0;
@@ -79,14 +69,18 @@ function startCalc(){
 }
 
 function calc(){
-	one = $("input[name=firstBox]").val();
-	oneMult =15;
 
-	two = $("input[name=secondBox]").val();
-	twoMult = 4;
+	one = $("input[name=firstBox]").val(); //Page Count
+	oneMult =10;
 
-	three = $("input[name=thirdBox]").val();
-	threeMult = 3;
+	two = $("input[name=secondBox]").val(); //Layout Change per page
+	twoMult = 5;
+
+	three = $("input[name=thirdBox]").val(); //Product Count
+	threeMult = 4;
+
+	four = $("input[name=fourthBox]").val(); //Option Set Count
+	fourMult = 3;	
 
 	// Calculate Total
 	$("#total").html(
@@ -106,4 +100,58 @@ function calc(){
 
 function stopCalc(){
 	clearInterval(interval);
+}
+
+
+// ============================================
+// 		 	    Quote Generation
+// ============================================
+function generateQuote() {
+	var genQuote = document.getElementById("generated-quote");
+	var colorChangeYes = document.getElementById("colorChangeYes");
+	var colorChangeNo = document.getElementById("colorChangeNo");
+	var storeYes = document.getElementById("storeYes");
+	var storeNo = document.getElementById("storeNo");
+	
+	var colorChange = 0;
+	var store = 0;
+
+	//Variable Adjustment for Notation
+	one = (one * oneMult);
+	two = (two * twoMult);
+	three = (three * threeMult);
+	four = (four * fourMult);
+	five = three;
+	six = four;
+
+	// Radio Condition Check
+	if ($('#colorChangeYes').prop("checked")) {
+		colorChange = $('#colorChangeYes').attr("value");
+	} else {
+		colorChange = $('#colorChangeNo').attr("value");
+	};
+
+	if ($('#storeYes').prop("checked")) {
+		store = $('#storeYes').attr("value");
+	} else {
+		store = $('#storeNo').attr("value");
+	};
+
+	genQuote.innerHTML ="&lt;table style=&quot;border-collapse: collapse; border-spacing: 0;&quot;&gt;&lt;tr&gt;&lt;th style=&quot;font-family: Arial, sans-serif; font-size: 14px; font-weight: normal; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal; font-weight: bold; font-size: 22px; background-color: #ef6c0f; color: #ffffff; text-align: center&quot; colspan=&quot;2&quot;&gt;Work Estimate&lt;/th&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;Initial Setup&lt;/td&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;"+baseline+"&lt;/td&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;Pages&lt;/td&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;"+one+"&lt;/td&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;Pages w/ Layout Changes&lt;/td&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;"+two+"&lt;/td&gt; &lt;/tr&gt;&lt;tr&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;Color Change?&lt;/td&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;"+colorChange+"&lt;/td&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;Store?&lt;/td&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;"+store+"&lt;/td&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;Products&lt;/td&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;"+five+"&lt;/td&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;Option Sets&lt;/td&gt;&lt;td style=&quot;font-family: Arial, sans-serif; font-size: 14px; padding: 10px 5px; border-style: solid; border-width: 1px; overflow: hidden; word-break: normal;&quot; class=&quot;tg-yw4l&quot;&gt;"+six+"&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;";
+}
+
+
+// ============================================
+//     Conditional Accordion Functionality
+// ============================================
+function openAccordion(){
+	if ($('#storeYes').parent().hasClass('active')) {
+		$('#storeChildren').collapse('show');
+		$('#storeChildren').addClass("in");
+	} else if ($('#storeNo').parent().hasClass('active')) {
+		$('#storeChildren').collapse('hide');
+		$('#storeChildren').removeClass("in");
+	} else{
+		$('#storeChildren').collapse('hide');
+	};
 }
