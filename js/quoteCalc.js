aa8b326409e1517676e9be177d14849ec7331b8f
multimediate <jake@multimediate.us>
2015-12-15 18:18:57 -0700

    v1.2.3
    Changed HTML export to reflect input rather than calculated values; UI
    color changes

--- js/quoteCalc.js
+++ js/quoteCalc.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////
 // ABOUT: Quote Calculator
 // CREATED BY: Jacob Wiedmann
-// VERSION: 1.2.2
+// VERSION: 1.2.3
 // COPYRIGHT: GoDaddy Operating Company, LLC All Rights Reserved
 ////////////////////////////////////////////////////////////
 
@@ -52,6 +52,7 @@ var fourMult = 0;
 
 var fiveMult = 0; //extra
 var sixMult = 0; //extra
+
 
 
 function startCalc(){
@@ -110,28 +111,44 @@ function generateQuote() {
 
 	// Radio Condition Check
 	if ($('#colorChangeYes').prop("checked")) {
-		colorChange = $('#colorChangeYes').attr("value");
+		// colorChange = $('#colorChangeYes').attr("value");
+		colorChange = "Yes";
 	} else {
-		colorChange = $('#colorChangeNo').attr("value");
+		// colorChange = $('#colorChangeNo').attr("value");
+		colorChange = "No";
 	};
 
 	if ($('#storeYes').prop("checked")) {
-		store = $('#storeYes').attr("value");
+		// store = $('#storeYes').attr("value");
+		store = "Yes";
 	} else {
-		store = $('#storeNo').attr("value");
+		// store = $('#storeNo').attr("value");
+		store = "No";
 	};
 
 
 	//Calculated Line Items for Notation Purposes
-	rowOne = baseline; 					//  Initial Setup
-	rowTwo = (one * oneMult); 			//  Page Count
-	rowThree = (two * twoMult); 		//  Layout Changes
+	rowOne = baseline + " minutes";		//  Initial Setup
+	rowTwo = one;						//  Page Count
+	rowThree = two; 					//  Layout Changes
 	rowFour = colorChange; 				//  Color Change?
 	rowFive = store; 					//  Store?
-	rowSix = (three * threeMult);  		//  Product Count
-	rowSeven = (four * fourMult);		//  Option Sets
-	quoteTotal = (+rowOne + +rowTwo + +rowThree + +rowFour + +rowFive + +rowSix + +rowSeven);
-
+	rowSix = three;  					//  Product Count
+	rowSeven = four;					//  Option Sets
+	// quoteTotal = (+rowOne + +rowTwo + +rowThree + +rowFour + +rowFive + +rowSix + +rowSeven);
+	quoteTotal = (
+		Math.ceil (
+		baseline +
+		(one * oneMult) + 
+		(two * twoMult) + 
+		(three * threeMult) + 
+		(four * fourMult) +
+		(five * fiveMult) + 
+		(six * sixMult) +
+		+ radioTotal
+		)
+	)
+	+ " minutes";
 
 	// Generate HTML
 	genQuote.innerHTML ="&lt;table class=&quot;tg&quot; style=&quot;border-collapse: collapse;border-spacing: 0;margin: 0px auto;width: 100%;&quot;&gt; &lt;tr&gt; &lt;th class=&quot;tg-otoz&quot; colspan=&quot;2&quot; style=&quot;font-family: Arial, sans-serif;font-size: 24px;font-weight: bold;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ef6c0f;color: #ffffff;text-align: center;border-color: #000;&quot;&gt;Work Estimate&lt;/th&gt; &lt;/tr&gt; &lt;tr&gt; &lt;td class=&quot;tg-5frq&quot; colspan=&quot;2&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;font-style: italic;text-align: center;vertical-align: top;&quot;&gt;Shown in Minutes&lt;/td&gt; &lt;/tr&gt; &lt;tr&gt; &lt;td class=&quot;tg-cxkv&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;&quot;&gt;Initial Setup&lt;/td&gt; &lt;td class=&quot;tg-3we0&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;vertical-align: top;&quot;&gt;" + rowOne + " &lt;/td&gt; &lt;/tr&gt; &lt;tr&gt; &lt;td class=&quot;tg-cxkv&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;&quot;&gt;Pages&lt;/td&gt; &lt;td class=&quot;tg-3we0&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;vertical-align: top;&quot;&gt;" + rowTwo + "&lt;/td&gt; &lt;/tr&gt; &lt;tr&gt; &lt;td class=&quot;tg-cxkv&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;&quot;&gt;Pages w/ Layout Changes&lt;/td&gt; &lt;td class=&quot;tg-3we0&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;vertical-align: top;&quot;&gt;" + rowThree + "&lt;/td&gt; &lt;/tr&gt; &lt;tr&gt; &lt;td class=&quot;tg-cxkv&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;&quot;&gt;Color Change?&lt;/td&gt; &lt;td class=&quot;tg-3we0&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;vertical-align: top;&quot;&gt;" + rowFour + "&lt;/td&gt; &lt;/tr&gt; &lt;tr&gt; &lt;td class=&quot;tg-cxkv&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;&quot;&gt;Store?&lt;/td&gt; &lt;td class=&quot;tg-3we0&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;vertical-align: top;&quot;&gt;" + rowFive + "&lt;/td&gt; &lt;/tr&gt; &lt;tr&gt; &lt;td class=&quot;tg-3we0&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;vertical-align: top;&quot;&gt;Products&lt;/td&gt; &lt;td class=&quot;tg-3we0&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;vertical-align: top;&quot;&gt;" + rowSix + "&lt;/td&gt; &lt;/tr&gt; &lt;tr&gt; &lt;td class=&quot;tg-3we0&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;vertical-align: top;&quot;&gt;Option Sets&lt;/td&gt; &lt;td class=&quot;tg-3we0&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;background-color: #ffffff;vertical-align: top;&quot;&gt;" + rowSeven + "&lt;/td&gt; &lt;/tr&gt; &lt;tr&gt; &lt;td class=&quot;tg-2ib9&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;font-weight: bold;background-color: #008a32;color: #ffffff;vertical-align: top;border-color: #000;&quot;&gt;Total Time&lt;/td&gt; &lt;td class=&quot;tg-2ib9&quot; style=&quot;font-family: Arial, sans-serif;font-size: 14px;padding: 10px 16px;border-style: solid;border-width: 1px;overflow: hidden;word-break: normal;font-weight: bold;background-color: #008a32;color: #ffffff;vertical-align: top;border-color: #000;&quot;&gt;" + quoteTotal + "&lt;/td&gt; &lt;/tr&gt; &lt;/table&gt;";
